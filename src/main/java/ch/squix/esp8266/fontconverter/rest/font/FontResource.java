36f56d3d42f83f988fc9184c32f81f79c3b4ba85
Daniel Eichhorn <dani.eichhorn@squix.ch>
2018-03-10 06:43:00 +0100

    Add debugging output

--- src/main/java/ch/squix/esp8266/fontconverter/rest/font/FontResource.java
+++ src/main/java/ch/squix/esp8266/fontconverter/rest/font/FontResource.java
@@ -7,21 +7,28 @@ import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
+import ch.squix.esp8266.fontconverter.rest.*;
 import org.restlet.resource.Get;
 import org.restlet.resource.ServerResource;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class FontResource extends ServerResource {
 
+    private static Logger logger = LoggerFactory.getLogger(FontResource.class);
+
     @Get(value = "json")
     public List<FontDto> execute() throws FontFormatException, IOException {
+
         String fontFamily = (String) this.getRequestAttributes().get("fontFamily");
+        logger.info("loading font types for {}", fontFamily);
         List<FontDto> fonts = new ArrayList<>();
         GraphicsEnvironment graphicEnvironment = GraphicsEnvironment.getLocalGraphicsEnvironment();
         for (Font font : graphicEnvironment.getAllFonts()) {
             if (fontFamily == null || fontFamily.equals(font.getFamily())) {
                 FontDto dto = new FontDto();
-                dto.setName("YY" + font.getFontName());
-                dto.setFontFamily("XX" + font.getFamily());
+                dto.setName(font.getFontName());
+                dto.setFontFamily(font.getFamily());
                 dto.setPlain(font.isPlain());
                 dto.setItalic(font.isItalic());
                 dto.setBold(font.isBold());
--- src/main/java/ch/squix/esp8266/fontconverter/rest/fontfamily/FontFamilyResource.java
+++ src/main/java/ch/squix/esp8266/fontconverter/rest/fontfamily/FontFamilyResource.java
@@ -6,19 +6,25 @@ import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
+import ch.squix.esp8266.fontconverter.rest.font.FontResource;
 import org.restlet.resource.Get;
 import org.restlet.resource.ServerResource;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class FontFamilyResource extends ServerResource {
 
+    private static Logger logger = LoggerFactory.getLogger(FontFamilyResource.class);
+
     @Get(value = "json")
     public List<FontFamilyDto> execute() throws FontFormatException, IOException {
+        logger.info("Load all font family names");
         List<FontFamilyDto> fonts = new ArrayList<>();
         GraphicsEnvironment graphicEnvironment = GraphicsEnvironment.getLocalGraphicsEnvironment();
 
         for (String fontFamilyName : graphicEnvironment.getAvailableFontFamilyNames()) {
             FontFamilyDto dto = new FontFamilyDto();
-            dto.setName(fontFamilyName);
+            dto.setName("XX" + fontFamilyName);
             fonts.add(dto);
         }
         return fonts;
